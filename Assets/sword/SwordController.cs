f6d903895c11aa69cf1010933e67cc21d69e142e
MaiThiLy <lymthe153498@fpt.edu.vn>
2023-03-03 22:51:46 +0700

    lymt_0303
    vu khi sung

---
+++ Assets/sword/SwordController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    int ATKBase = 3;
+    int ATKSpeed = 1;
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        time-=Time.deltaTime;
+        if(time <= 0)
+        {
+            //Tấn công 1 lần (Di chuyển 3/4 hình tròn)
+
+            time = 1;
+            Debug.Log("Đã bị tấn công" + ATKBase);
+        }
+        
+    }
+    //khi đối tượng chạm vào quái 
+    void OnCollisionEnter2D(Collision2D col)
+    {
+      
+        
+    }
+}
