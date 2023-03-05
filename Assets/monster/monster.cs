eaa2b6571abebd299f799aac4d456a1af34eea4f
Duoc Nguyen <duocng.2409@gmail.com>
2023-03-05 22:20:16 +0700

    commit code spawn monster

---
+++ Assets/monster/monster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster : MonoBehaviour
+{
+    public Transform destination; // Khai báo biến destination kiểu Vector3
+
+    public float speed = 1F; // Khai báo tốc độ di chuyển của Object
+
+    void Start()
+    {
+        destination = GameObject.Find("Circle").transform;   
+    }
+
+    // Update được gọi mỗi frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime; // Tính toán khoảng cách Object di chuyển trong mỗi frame
+        transform.position = Vector3.MoveTowards(transform.position, destination.position, step); // Di chuyển Object đến vị trí đích
+    }
+}
